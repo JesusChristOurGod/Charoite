44f3532e494c25e47635f21b0aac148e6455cdd0
JesusChristOurGod <goryachev.web@gmail.com>
2021-06-12 20:53:37 +0300

    Initial Commit

---
+++ Charoite/CharoiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CharoiteApp.swift
+//  Charoite
+//
+//  Created by Горячев Александр on 12.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct CharoiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Charoite/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Charoite
+//
+//  Created by Горячев Александр on 12.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
